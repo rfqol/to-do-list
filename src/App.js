f3b348b63cdc063a1ebf235105c34526790da8d2
Edil Stamaliev <stamalieved@gmail.com>
2022-04-20 18:43:03 +0600

    Markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello</h1>;
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Task 1" },
+    { id: 2, title: "Task 2" },
+  ]);
+
+  return (
+    <div className="App">
+      <h1>To-do List</h1>
+      <p>0/0 completed</p>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
